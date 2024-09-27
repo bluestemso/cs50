2411554361181a7999118a4f416fab40ab08950a
taylor <taylor@bluestem.solutions>
2024-09-27 16:30:38 -0500

    Finished lecture 4

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -11,7 +11,24 @@ def index(request):
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
     passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": passengers
+        "passengers": passengers,
+        "non_passengers": non_passengers
+    })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return render(request, "flights/flight.html", {
+            "flight": flight,
+            "passengers": flight.passengers.all(),
+            "message": "Added."
+        })
+    return render(request, "flights/book.html", {
+        "flights": Flight.objects.all(),
+        "passengers": Passenger.objects.all()
     })
